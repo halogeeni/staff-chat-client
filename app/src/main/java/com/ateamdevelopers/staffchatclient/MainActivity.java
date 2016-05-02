94c6dcacb01307829bce279058bb223830cf03a6
Aleksi Rasio <aleksi.rasio@metropolia.fi>
2016-05-02 12:27:11 +0300

    ContentProvider fixes

--- app/src/main/java/com/ateamdevelopers/staffchatclient/MainActivity.java
+++ app/src/main/java/com/ateamdevelopers/staffchatclient/MainActivity.java
@@ -1,5 +1,10 @@
 package com.ateamdevelopers.staffchatclient;
 
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,8 +12,16 @@ import android.os.Handler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,38 +29,62 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private final String TAG = "MainActivity";
+    private final int MESSAGE_LOADER_ID = 0x01;
+
+    private final int currentUser = 0;
 
     // action bar title - we will eventually migrate to this
     private String mTitle;
 
+    private String[] projection = {
+            MessageContract.MessageEntry._ID,
+            MessageContract.MessageEntry.COLUMN_NAME_FROM_USER,
+            MessageContract.MessageEntry.COLUMN_NAME_TO_USER,
+            MessageContract.MessageEntry.COLUMN_NAME_TO_GROUP,
+            MessageContract.MessageEntry.COLUMN_NAME_BODY,
+            MessageContract.MessageEntry.COLUMN_NAME_TIMESTAMP
+    };
+
     // TODO implement navigation drawer
 
     // REST URL - should be eventually the root, e.g. http://10.0.2.2:8080/StaffChat/webresources/
-    private Uri restEndpointURI;
-    private MessageAdapter mAdapter;
-    private List<Message> mMessages;
+    private final String url = "http://10.0.2.2:8080/StaffChat/webresources/messages/broadcast";
+
+    private Uri mEndpointURI;
+    private MessageDatabaseHelper mDbHelper;
+    private SimpleCursorAdapter mAdapter;
+    private List<Message> mLastMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        restEndpointURI = Uri.parse("http://10.0.2.2:8080/StaffChat/webresources/messages/broadcast");
-        ListView messageListView = (ListView) findViewById(R.id.messageList);
+        // flush the database on login
+        mDbHelper = new MessageDatabaseHelper(this);
+        mDbHelper.initDb();
+        mDbHelper.close();
 
-        mMessages = new ArrayList<>();
-        mAdapter = new MessageAdapter(this, mMessages);
-        messageListView.setAdapter(mAdapter);
+        mLastMessages = new ArrayList<>();
 
-        callAsyncTask();
+        mAdapter = new SimpleCursorAdapter(this,
+                R.layout.message_list_item, null,
+                new String[] { MessageContract.MessageEntry.COLUMN_NAME_FROM_USER,
+                        MessageContract.MessageEntry.COLUMN_NAME_BODY,
+                        MessageContract.MessageEntry.COLUMN_NAME_TIMESTAMP },
+                new int[] { R.id.messageUsername , R.id.messageBody, R.id.messageTimestamp }, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        ListView mListView = (ListView) findViewById(R.id.messageList);
+        mListView.setAdapter(mAdapter);
+        startPollingTask();
+        getLoaderManager().initLoader(MESSAGE_LOADER_ID, null, this);
     }
 
     // 2 sec polling timer
-    protected void callAsyncTask() {
+    protected void startPollingTask() {
         final Handler handler = new Handler();
         Timer timer = new Timer();
         TimerTask doAsynchronousTask = new TimerTask() {
@@ -56,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 handler.post(new Runnable() {
                     public void run() {
                         try {
-                            new AsyncListViewLoader().execute(restEndpointURI);
+                            new DownloadXmlTask().execute(url);
                         } catch (Exception e) {
                             // TODO Auto-generated catch block
                         }
@@ -67,60 +104,98 @@ public class MainActivity extends AppCompatActivity {
         timer.schedule(doAsynchronousTask, 0, 2000); //execute every 2000 ms
     }
 
-    private class AsyncListViewLoader extends AsyncTask<Uri, Void, List<Message>> {
-        @Override
-        protected void onPostExecute(List<Message> result) {
-            super.onPostExecute(result);
-            // update listview only if new messages have been fetched
-            Log.d(TAG, "messages now: " + result.size());
-            if(result != null) {
-                if(result.size() > mMessages.size()) {
-                    Log.d(TAG, "new results available - updating UI");
-                    // update UI only if new messages have been fetched
-                    mMessages.clear();
-                    mMessages.addAll(result);
-                    mAdapter.notifyDataSetChanged();
-                    Log.d(TAG, "listadapter getCount() now: " + mAdapter.getCount());
-                }
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected List<Message> doInBackground(Uri... params) {
-            Log.d(TAG, "in doInBackground()");
-            List<Message> result = new ArrayList<Message>();
-            HttpURLConnection conn;
-
-            try {
-                // parse string to url object
-                URL u = new URL(params[0].toString());
-                // open connection to url
-                conn = (HttpURLConnection) u.openConnection();
-                conn.setRequestMethod("GET");
-                conn.connect();
-                // get response code from connection in order to avoid unnecessary XML parsing
-                // ideally the server should return 304 NOT MODIFIED response in case there is no new content
-                // but the backend is not set up that way, at the moment
-                int response = conn.getResponseCode();
-                if(response == 200) {
-                    Log.d(TAG, "server response was 200 OK - parsing data");
-                    // OK - connection OK & new content is available
-                    XmlMessageParser parser = new XmlMessageParser();
-                    return parser.parse(conn.getInputStream());
-                }
-                conn.disconnect();
-            } catch(Throwable t) {
-                t.printStackTrace();
-            }
-
-            return null;
-        }
-
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, MessageContentProvider.CONTENT_URI, projection, null, null, null);
     }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        //mAdapter.notifyDataSetChanged();
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadXml(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+    }
+
+    private String downloadXml(String urlString) throws XmlPullParserException, IOException {
+        Log.d(TAG, "in downloadXml()");
+
+        InputStream stream = null;
+        XmlMessageParser messageParser = new XmlMessageParser();
+        String result = "";
+
+        try {
+            // connect to the server & get stream
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+            conn.getInputStream();
+            stream = new BufferedInputStream(conn.getInputStream());
+
+            // not the most elegant solution, but works for now
+            List<Message> messageList = messageParser.parse(stream);
+            Log.d(TAG, "in downloadXml - mLastMessages now: " + mLastMessages);
+            if(mLastMessages.isEmpty()) {
+                mLastMessages.addAll(messageList);
+                // Create a new map of values, where column names are the keys
+                ContentValues values = new ContentValues();
+                for (Message m : messageList) {
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_FROM_USER, m.getFromUserId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TO_USER, m.getToUserId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TO_GROUP, m.getToGroupId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_BODY, m.getBody());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TIMESTAMP, m.getTimestamp());
+                    getContentResolver().insert(MessageContentProvider.CONTENT_URI, values);
+                }
+            } else if(!messageList.containsAll(mLastMessages)) {
+                Log.d(TAG, "fetched message list contained new entries");
+
+                List<Message> tempMessageList = new ArrayList<>(messageList);
+
+                messageList.removeAll(mLastMessages);
+                ContentValues values = new ContentValues();
+                for (Message m : messageList) {
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_FROM_USER, m.getFromUserId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TO_USER, m.getToUserId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TO_GROUP, m.getToGroupId());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_BODY, m.getBody());
+                    values.put(MessageContract.MessageEntry.COLUMN_NAME_TIMESTAMP, m.getTimestamp());
+                    getContentResolver().insert(MessageContentProvider.CONTENT_URI, values);
+                }
+
+                mLastMessages.clear();
+                mLastMessages.addAll(tempMessageList);
+            }
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDbHelper.close();
+    }
+
 
 }
--- app/src/main/java/com/ateamdevelopers/staffchatclient/MessageContract.java
+++ app/src/main/java/com/ateamdevelopers/staffchatclient/MessageContract.java
@@ -3,10 +3,9 @@ package com.ateamdevelopers.staffchatclient;
 import android.provider.BaseColumns;
 
 public final class MessageContract {
-    // Inner class that defines the table contents
     public static abstract class MessageEntry implements BaseColumns {
         public static final String TABLE_NAME = "message";
-        //public static final String COLUMN_NAME_ENTRY_ID = "id";
+
         public static final String COLUMN_NAME_BODY = "body";
         public static final String COLUMN_NAME_FROM_USER = "fromUserId";
         public static final String COLUMN_NAME_TO_USER = "toUserId";
